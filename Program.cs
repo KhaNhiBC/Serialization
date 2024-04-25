b4f419135e10c16d19bb6ec7fce9484c86b7c44c
KhaNhi <KhaNhi@users.noreply.github.com>
2024-04-25 21:23:28 +0700

    Serialization

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "Nguyen Thi Buom";
+        StoreString(name);
+        Console.WriteLine(ReadString());
+    }
+    static void StoreString(string linkvalue)
+    {
+
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/"+"Link.txt";
+        Console.WriteLine(path);
+        /* FileInfo fileinfo = new FileInfo(path);
+        fileinfo.Create(); */
+        //
+        FileStream fs = new FileStream(path, FileMode.Create);
+        StreamWriter streamWriter = new StreamWriter(fs,System.Text.Encoding.UTF8);
+        streamWriter.Write(linkvalue);
+        streamWriter.Flush();
+        fs.Close();
+    }
+    static string ReadString()
+    {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)+"/"+"Link.txt";
+        FileStream fs = new FileStream(path, FileMode.Open);
+        StreamReader sr = new StreamReader(fs,System.Text.Encoding.UTF8);
+        string giatri = sr.ReadToEnd();
+        sr.Close();
+        return giatri;
+    }
+}
